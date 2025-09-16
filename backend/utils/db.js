022bcd9632b82592751b5fc97b315601bd0ea832
Kabir-Iqbal <kabirhussain0343837@gmail.com>
2025-09-17 01:44:23 +0500

    fixing errors

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -6,6 +6,7 @@ dotenv.config();
 
 const connectDB = async () => {
     try{
+        console.log("🔍 MONGO_URI:", process.env.MONGO_URI); // Debug
         await mongoose.connect(process.env.MONGO_URI)
         console.log("Connected to MongoDB");
     }catch(error){
