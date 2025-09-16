f5e4018b42e971a391aa2ba7741e5efbb17754e8
Kabir-Iqbal <kabirhussain0343837@gmail.com>
2025-09-17 02:02:10 +0500

    updated code

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,17 +1,39 @@
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+
+// const connectDB = async () => {
+//     try{
+//         console.log("🔍 MONGO_URI:", process.env.MONGO_URI); // Debug
+//         await mongoose.connect(process.env.MONGO_URI)
+//         console.log("Connected to MongoDB");
+//     }catch(error){
+//         console.log("Error connecting to MongoDB", error);
+//     }
+// }
+
+// export default connectDB;
+
+
+
+
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-dotenv.config();
-
 
 const connectDB = async () => {
-    try{
-        console.log("🔍 MONGO_URI:", process.env.MONGO_URI); // Debug
-        await mongoose.connect(process.env.MONGO_URI)
-        console.log("Connected to MongoDB");
-    }catch(error){
-        console.log("Error connecting to MongoDB", error);
-    }
-}
+  if (mongoose.connection.readyState >= 1) {
+    return;
+  }
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ Connected to MongoDB");
+  } catch (error) {
+    console.log("❌ MongoDB connection error:", error);
+  }
+};
 
 export default connectDB;
